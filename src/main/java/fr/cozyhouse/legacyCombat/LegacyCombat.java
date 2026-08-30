379a7fb89d79560e23b8f572f70560fb6850716e
Void <84667427+voidunnamed@users.noreply.github.com>
2026-08-30 10:44:39 +0200

    Project-1

--- src/main/java/fr/cozyhouse/legacyCombat/LegacyCombat.java
+++ src/main/java/fr/cozyhouse/legacyCombat/LegacyCombat.java
@@ -1,6 +1,7 @@
 package fr.cozyhouse.legacyCombat;
 
 import fr.cozyhouse.legacyCombat.commands.CommandManager;
+import fr.cozyhouse.legacyCombat.listener.EntityDamageByEntityListener;
 import fr.cozyhouse.legacyCombat.listener.PlayerJoinListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,12 +10,16 @@ import java.util.Objects;
 
 public final class LegacyCombat extends JavaPlugin {
 
+    static public LegacyCombat plugin;
+
     @Override
     public void onEnable() {
+        plugin = this;
         Bukkit.getLogger().info("LegacyCombat activated");
-        Bukkit.getLogger().info("Version : " + Bukkit.getVersion());
+        Bukkit.getLogger().info("Version : " + plugin.getDescription().getVersion());
         Objects.requireNonNull(this.getCommand("legacycombat")).setExecutor(new CommandManager());
         this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+        this.getServer().getPluginManager().registerEvents(new EntityDamageByEntityListener(), this);
         // Plugin startup logic
     }
 
---
+++ src/main/java/fr/cozyhouse/legacyCombat/listener/EntityDamageByEntityListener.java
@@ -0,0 +1,42 @@
+package fr.cozyhouse.legacyCombat.listener;
+
+import fr.cozyhouse.legacyCombat.LegacyCombat;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.security.Timestamp;
+import java.sql.Time;
+import java.util.Objects;
+
+public class EntityDamageByEntityListener implements Listener {
+    int count;
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e){
+        Bukkit.getLogger().info(String.valueOf(e.getDamage()));
+        if (e.getDamager() instanceof Player player){
+            Bukkit.getLogger().info(String.valueOf(player.isSprinting()));
+            Bukkit.getLogger().info(String.valueOf(Objects.requireNonNull(player.getAttribute(Attribute.ATTACK_SPEED)).getValue()));
+        }
+        Bukkit.getLogger().info(String.valueOf(e.getDamager().getFallDistance() > 0));
+        count = 0;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                count++;
+                if (count < 3) {
+                    Bukkit.getLogger().info(e.getEntity().getLocation().toString());
+                } else {
+                    this.cancel();
+                }
+            }
+        }.runTaskTimer(LegacyCombat.plugin, 0L, 20L);
+    }
+}
